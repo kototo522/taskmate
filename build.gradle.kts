a115b44ae908935bbe7d7988ea5d3a1c7d686e52
kototo522 <huyejiben@gmail.com>
2024-09-27 16:21:13 +0900

    add: spotlessの追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,19 @@
+import org.jetbrains.kotlin.builtins.StandardNames.FqNames.target
+import org.jetbrains.kotlin.js.translate.context.Namer.kotlin
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.spotless)
+}
+
+spotless {
+    ratchetFrom("origin/main")
+    kotlin {
+        target("**/*.kt")
+        targetExclude("$buildDir/**/*.kt")
+        targetExclude("bin/**/*.kt")
+        ktlint("0.49.1")
+    }
 }
