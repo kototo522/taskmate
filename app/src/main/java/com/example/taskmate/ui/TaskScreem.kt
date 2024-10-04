d8ef2558e44d2699e80fbfc97ff418ab621c796e
kototo522 <huyejiben@gmail.com>
2024-10-04 14:58:15 +0900

    add: floatingButtonの追加

--- app/src/main/java/com/example/taskmate/ui/TaskScreem.kt
+++ app/src/main/java/com/example/taskmate/ui/TaskScreem.kt
@@ -3,20 +3,38 @@ package com.example.taskmate.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmate.R
 import com.example.taskmate.ui.appBar.MainTaskMateAppBar
 
 @Composable
 fun TaskScreen(navToSettingScreen: () -> Unit) {
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             MainTaskMateAppBar(navToSettingScreen, Modifier)
         },
+        floatingActionButton = {
+            Box(modifier = Modifier.padding(end = 8.dp)) {
+                FloatingActionButton(
+                    onClick = {},
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    elevation = FloatingActionButtonDefaults.elevation(8.dp)
+                ) {
+                    Text(text = context.getString(R.string.add_icon), fontSize = 24.sp)
+                }
+            }
+        }
     ) { innerPadding ->
         Box(
             modifier = Modifier
@@ -24,7 +42,7 @@ fun TaskScreen(navToSettingScreen: () -> Unit) {
                 .fillMaxSize(),
             contentAlignment = Alignment.Center,
         ) {
-            Text("MyPageScreen", style = MaterialTheme.typography.bodyLarge)
+            Text("TaskScreen", style = MaterialTheme.typography.bodyLarge)
         }
     }
 }
