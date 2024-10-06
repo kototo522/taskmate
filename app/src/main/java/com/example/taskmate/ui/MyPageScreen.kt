ddac38ce2401b7c343ed0aabc84d5202927ec89f
kototo522 <huyejiben@gmail.com>
2024-10-06 14:54:45 +0900

    add: MyPageのUI作成

--- app/src/main/java/com/example/taskmate/ui/MyPageScreen.kt
+++ app/src/main/java/com/example/taskmate/ui/MyPageScreen.kt
@@ -1,18 +1,26 @@
 package com.example.taskmate.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmate.R
 import com.example.taskmate.ui.appBar.MainTaskMateAppBar
 
 @Composable
 fun MyPageScreen(navToSettingScreen: () -> Unit) {
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             MainTaskMateAppBar(navToSettingScreen, Modifier)
@@ -24,7 +32,88 @@ fun MyPageScreen(navToSettingScreen: () -> Unit) {
                 .fillMaxSize(),
             contentAlignment = Alignment.Center,
         ) {
-            Text("MyPageScreen", style = MaterialTheme.typography.bodyLarge)
+            Box(
+                modifier = Modifier
+                    .padding(40.dp)
+                    .fillMaxSize()
+                    .background(
+                        color = MaterialTheme.colorScheme.onSecondary,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Box(
+                        contentAlignment = Alignment.TopEnd,
+                        modifier = Modifier
+                            .size(140.dp)
+                    ) {
+                        Text(
+                            text = context.getString(R.string.add_icon),
+                            fontSize = 28.sp,
+                            modifier = Modifier
+                                .clickable { }
+                                .padding(8.dp)
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.account),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .height(140.dp)
+                                .width(140.dp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(40.dp))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "ユーザ名",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+
+                    Text(
+                        text = "ユーザネーム",
+                        fontSize = 24.sp,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(4.dp)
+                    )
+                    Spacer(modifier = Modifier.height(40.dp))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "所属グループ",
+                            style = MaterialTheme.typography.bodyLarge,
+                            modifier = Modifier
+                                .padding(4.dp)
+                                .weight(5f)
+                        )
+                        Text(
+                            text = context.getString(R.string.add_icon),
+                            fontSize = 24.sp,
+                            modifier = Modifier
+                                .clickable {}
+                                .weight(1f)
+                                .padding(4.dp)
+                        )
+                    }
+                    Row {
+                        Text(
+                            text = "タグ",
+                            fontSize = 24.sp,
+                            style = MaterialTheme.typography.bodyLarge,
+                            modifier = Modifier.padding(4.dp)
+                        )
+                    }
+                }
+            }
         }
     }
 }
