735a17cb03b4210f7ce8d0a7b52e880ca75a5533
kototo522 <huyejiben@gmail.com>
2024-10-10 15:47:55 +0900

    change: taskCardのUI変更

--- app/src/main/java/com/example/taskmate/ui/task/TaskCard.kt
+++ app/src/main/java/com/example/taskmate/ui/task/TaskCard.kt
@@ -1,11 +1,9 @@
 package com.example.taskmate.ui.task
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -13,7 +11,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -41,20 +38,25 @@ fun TaskCard(task: Task, modifier: Modifier = Modifier) {
                     text = task.title,
                     style =
                     TextStyle(
-                        fontSize = 18.sp,
-                        lineHeight = 18.sp,
+                        fontSize = 24.sp,
+                        lineHeight = 24.sp,
                         fontWeight = FontWeight(700),
                         color = MaterialTheme.colorScheme.onPrimaryContainer,
                         letterSpacing = 0.5.sp,
                     ),
-                    modifier = Modifier.padding(start = 20.dp, top = 8.dp),
+                    modifier = Modifier.padding(start = 20.dp, top = 14.dp),
                 )
-                Row {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp, end = 18.dp, bottom = 10.dp),
+                ) {
                     Text(
-                        text = "提出場所：WebClass",
-                        modifier = Modifier.padding(start = 20.dp, top = 4.dp),
-                        style =
-                        TextStyle(
+                        text = "技術者倫理哲学",
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 4.dp)
+                            .weight(1f),
+                        style = TextStyle(
                             fontSize = 14.sp,
                             lineHeight = 14.sp,
                             fontWeight = FontWeight(700),
@@ -63,16 +65,20 @@ fun TaskCard(task: Task, modifier: Modifier = Modifier) {
                         ),
                     )
 
+                    Text(
+                        text = "最終編集者: ことりん",
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 4.dp),
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            lineHeight = 14.sp,
+                            fontWeight = FontWeight(700),
+                            color = MaterialTheme.colorScheme.onPrimaryContainer,
+                            letterSpacing = 0.5.sp,
+                        ),
+                    )
                 }
-                Row(
-                    modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .align(Alignment.End)
-                        .height(40.dp),
-                ) {
 
-                }
             }
         }
     }
