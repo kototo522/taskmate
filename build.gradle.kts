3a183dadcfa1017cee2f0ced3f6e706d64181966
kototo522 <huyejiben@gmail.com>
2024-10-11 11:33:47 +0900

    add: dagger, hiltの導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.spotless)
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.hilt) apply false
 }
 
 spotless {
